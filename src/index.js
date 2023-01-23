ac40797834c7d8479ebf14c56dc2fd86b9f4088e
Guuzzeji <5lh8dl440f@gmail.com>
2023-01-22 17:16:12 -0800

    Setting format to null
    This will force to find the max format by default

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ class RedditVideoDL {
         }
     }
 
-    async createVideo({ format, setLogging, setLogger, setProgress }) {
+    async createVideo({ format = null, setLogging, setLogger, setProgress }) {
         let videoFormat;
 
         try {

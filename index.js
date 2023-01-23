7c2040cd0640f7f9ad4f19bf55c55a35c6cbc143
Guuzzeji <5lh8dl440f@gmail.com>
2023-01-22 16:51:32 -0800

    Testing error feature

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ exports.RedditVideoDL = require("./src/index");
 const RedditVideoDL = require("./src/index");
 
 async function main() {
-    let video1 = await RedditVideoDL.initialize("https://www.reddit.com/r/IndieDev/comments/10hgvjq/vr_has_been_punishing_for_particles");
+    let video1 = await RedditVideoDL.initialize("https://www.reddit.com/r/videos/comments/1055vt8/youtube_is_run_by_fools_prozd");
     console.log(await video1.createVideo());
 }
 

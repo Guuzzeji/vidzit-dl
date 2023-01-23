997c2b41aa1719d63729d994eca395a9e36272ff
Guuzzeji <5lh8dl440f@gmail.com>
2023-01-22 16:25:40 -0800

    Using ffmpegProcessing.js in class

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const redditFetch = require('./src/redditFetch').fetchUrls;
-const { createFFmpeg, fetchFile } = require('@ffmpeg/ffmpeg');
-const fs = require('fs');
 
+const createVideo = require('./src/ffmpegProcessing').createVideo;
 
 class RedditVideoDL {
     #videoDashFiles;
-    #ffmpeg;
 
     static async initialize(redditURL) {
         let dashInfo = await redditFetch(redditURL);
@@ -14,9 +12,6 @@ class RedditVideoDL {
 
     constructor(dashInfo) {
         this.videoDashFiles = dashInfo;
-        this.ffmpeg = createFFmpeg({
-            log: true
-        });
     }
 
     get videoStreamsInfo() {
@@ -35,23 +30,13 @@ class RedditVideoDL {
         return null;
     }
 
-
     async createVideo(format) {
         let videoFormat = this.findFormatVideo(format);
 
-        await this.ffmpeg.load();
-
-        this.ffmpeg.FS('writeFile', 'video.mp4', await fetchFile(videoFormat.url));
-        this.ffmpeg.FS('writeFile', 'aud.mp4', await fetchFile(this.videoDashFiles.dashContent.audio.url));
-
-        await this.ffmpeg.run('-i', 'video.mp4', '-i', 'aud.mp4', '-c:v', 'copy', '-c:a', 'copy', 'output.mp4');
-
-        //* Return Uint8Array which can be save as a file or something else\
-        try {
-            return this.ffmpeg.FS('readFile', 'output.mp4');
-        } finally {
-            this.ffmpeg.exit();
-        }
+        return await createVideo({
+            videoURL: videoFormat.url,
+            audioURL: this.videoDashFiles.dashContent.audio.url,
+        });
     }
 
 }
@@ -59,11 +44,7 @@ class RedditVideoDL {
 // Testing
 async function main() {
     let video1 = await RedditVideoDL.initialize("https://www.reddit.com/r/IndieDev/comments/10hgvjq/vr_has_been_punishing_for_particles");
-    console.log(video1.videoStreamsInfo);
-    `
-    // let video2 = new RedditVideoDL("https://www.reddit.com/r/IndieDev/comments/10hgvjq/vr_has_been_punishing_for_particles");
-    // await video2.initialize();
-    // fs.writeFileSync("./test.mp4", await video2.createVideo("720"));`;
+    console.log(await video1.createVideo("720"));
 }
 
 main();

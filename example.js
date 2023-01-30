171113886169c34770ed534cc6676b9a2b92bd0d
Guuzzeji <5lh8dl440f@gmail.com>
2023-01-30 11:20:22 -0800

    Adding more to example.js --> adding videoInfo

--- example.js
+++ example.js
@@ -2,7 +2,10 @@ const VidzDitDL = require("./index");
 
 async function main() {
     let video1 = await VidzDitDL.initialize("https://www.reddit.com/r/IndieDev/comments/10hgvjq/vr_has_been_punishing_for_particles");
-    console.log(await video1.createVideo());
+    console.log(video1.videoInfo);
+    console.log(await video1.createVideo({
+        setLogging: true,
+    }));
 }
 
 main();

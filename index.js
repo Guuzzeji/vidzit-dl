9fdbce42a05440a5ab6ea16472d43768b09199c6
Guuzzeji <5lh8dl440f@gmail.com>
2023-01-30 10:54:59 -0800

    Moving testing stuff to example.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
 exports.RedditVideoDL = require("./src/index");
-
-// Testing
-const RedditVideoDL = require("./src/index");
-
-async function main() {
-    let video1 = await RedditVideoDL.initialize("https://www.reddit.com/r/IndieDev/comments/10hgvjq/vr_has_been_punishing_for_particles");
-    console.log(await video1.createVideo());
-}
-
-main();

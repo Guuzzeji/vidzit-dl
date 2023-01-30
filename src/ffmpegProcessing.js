b81a9836eaf5f0dddae53d298440252fffe43c35
Guuzzeji <5lh8dl440f@gmail.com>
2023-01-30 10:46:58 -0800

    Relabel ffempg variables

--- src/ffmpegProcessing.js
+++ src/ffmpegProcessing.js
@@ -23,9 +23,9 @@ exports.createVideo = async function ({ videoURL, audioURL, setLogging = false, 
     await ffmpeg.load();
 
     ffmpeg.FS('writeFile', 'video.mp4', await fetchFile(videoURL));
-    ffmpeg.FS('writeFile', 'aud.mp4', await fetchFile(audioURL));
+    ffmpeg.FS('writeFile', 'audio.mp4', await fetchFile(audioURL));
 
-    await ffmpeg.run('-i', 'video.mp4', '-i', 'aud.mp4', '-c:v', 'copy', '-c:a', 'copy', 'output.mp4');
+    await ffmpeg.run('-i', 'video.mp4', '-i', 'audio.mp4', '-c:v', 'copy', '-c:a', 'copy', 'output.mp4');
 
     //* Return Uint8Array which can be save as a file or something else\
     try {

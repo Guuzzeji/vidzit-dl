b16f5964018099f7d62da02a1d8774eae227983a
Guuzzeji <5lh8dl440f@gmail.com>
2023-02-02 14:51:58 -0800

    Create a more detail example
    Example also help explain how API works

--- example.js
+++ example.js
@@ -4,7 +4,14 @@ async function main() {
     let video1 = await VidzitDL.initialize("https://www.reddit.com/r/IndieDev/comments/10hgvjq/vr_has_been_punishing_for_particles");
     console.log(video1.videoInfo);
     console.log(await video1.createVideo({
+        format: '480',
         setLogging: true,
+        setLogger: function (type, message) {
+            console.log(type + ": " + message);
+        },
+        setProgress: function (ratio) {
+            console.log(ratio);
+        }
     }));
 }
 
